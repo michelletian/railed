239e07d811a9adc0a3c4f6b204bc0980d42549d0
michellectian@gmail.com <michellectian@gmail.com>
2014-12-01 17:43:52 -0800

    seeds

--- railed/db/seeds.rb
+++ railed/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'Brian', city: 'Berkeley', review_count:1)
+Bathroom.create(name: 'Wheeler 1st Floor', location: 'Wheeler Hall', rating:3, cleanliness:3, comfort:3, smell:3, availability:3)
+Bathroom.create(name: 'Dwinelle Dungeon of Dreams', location: 'Dwinelle Hall', rating:1, cleanliness:1, comfort:1, smell:1, availability:1)
+
