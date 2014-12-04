cdf8d294b96ab17e4fcf77bc382b454dca12db65
michellectian@gmail.com <michellectian@gmail.com>
2014-12-04 01:51:13 -0800

    tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def reviews_not_nil
+  	assert_not_nil reviews
+  end
+
+  def test_should_not_save_post_without_title
+	  post = Post.new
+	  assert !post.save
+  end
 end
